acf9f0f278de76104489ec058fff3bad6bde2cc9
Hartlern <hartlern@mail.uc.edu>
2024-09-28 12:31:11 -0400

    added next line

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ public class Main
     public static void main( String[] args )
     {
         Scanner scan = new Scanner( System.in );
-        String menuChoice = "";
+        String menuChoice;
         String menu = "D - Democrat R - Republican I - Independent";
-        System.out.println("What is your political affiliation?\nD - Democrat R - Republican I - Independent");
+        System.out.println( "What is your political affiliation?\n" + menu );
         if( scan.hasNext() )
         {
             menuChoice = scan.next();
+            scan.nextLine();
+
             if( menuChoice.equalsIgnoreCase( "D" ) )
             {
                 System.out.println( "You chose Democratic Donkey" );
@@ -26,7 +28,7 @@ public class Main
             }
             else
             {
-                System.out.println("ERROR\nYou entered " + menuChoice + "\nYou must choose R,D, or I");
+                System.out.println( "ERROR\nYou entered " + menuChoice + "\nYou must choose R,D, or I" );
             }
         }
 
